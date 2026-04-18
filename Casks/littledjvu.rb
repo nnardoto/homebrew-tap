a76a12568f02e270f1551a1b219554293ab59cee
Nathanael Nardoto Batista <nnardoto@gmail.com>
2026-04-17 23:15:22 -0300

    Update version and URL for LittleDjVu cask

--- Casks/littledjvu.rb
+++ Casks/littledjvu.rb
@@ -1,8 +1,8 @@
 cask "littledjvu" do
-  version "0.1.0-beta"
+  version "0.1.0-alpha"
   sha256 "38b52e7957b5b1b52341b1b7986216d14593b6e9b753c918aa6d3885ad864c2f"
 
-  url "https://github.com/nnardoto/LittleDjVu/releases/download/v1.0/LittleDjVu-0.1.0-beta.zip"
+  url "https://github.com/nnardoto/LittleDjVu/releases/download/v0.1.0-alpha/LittleDjVu-0.1.zip"
   name "LittleDjVu"
   desc "Leitor de arquivos DjVu minimalista para macOS"
   homepage "https://github.com/nnardoto/LittleDjVu"
